bdd1a0fbbdf90c683645c6e62671030e15d2b60b
PushkarDesai-06 <pushkarjdesai@gmail.com>
2026-01-19 16:51:17 +0530

    initial commit

---
+++ src/main/java/com/pushkar/ecommersepayment/EcommersePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.ecommersepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommersePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommersePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pushkar/ecommersepayment/EcommersePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkar.ecommersepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommersePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
